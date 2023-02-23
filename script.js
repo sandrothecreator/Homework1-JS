f26f6bda7037864c6cc44a8be17a51914580fe1e
Alexander <88561565+sandrothecreator@users.noreply.github.com>
2023-02-23 16:05:53 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// პირველი ამოცანა
+
+let number = 100;
+
+if (number < 50){
+    console.log("ნაკლებია 50-ზე");
+}
+else if (number > 20){
+    console.log("მეტია 20-ზე");
+}
+else{
+    console.log("შეცდომა");
+}
+
+// მეორე ამოცანა
+
+let saxeli = "Mariami";
+
+if (saxeli == "Mariami"){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+// მესამე ამოცანა
+
+let letName = "Mariami"
+
+switch (letName){
+    case "Mariami":
+        console.log(true);
+        break;
+    default:
+        console.log(false);
+}
